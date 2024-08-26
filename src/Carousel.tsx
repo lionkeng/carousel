0292cd16cd37716a89a5becb9ac1f6bc3ac67081
Keng Lim <keng@anewgo.com>
2024-08-26 16:46:40 -0400

    add Carousel component

---
+++ src/Carousel.tsx
@@ -0,0 +1,61 @@
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  EffectCoverflow,
+} from 'swiper/modules'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+import 'swiper/css/effect-coverflow'
+
+export default function Carousel(): JSX.Element {
+  // if you want to use array
+  const slide_img = [
+    'https://swiperjs.com/demos/images/nature-1.jpg',
+    'https://swiperjs.com/demos/images/nature-2.jpg',
+    'https://swiperjs.com/demos/images/nature-3.jpg',
+    'https://swiperjs.com/demos/images/nature-4.jpg',
+    'https://swiperjs.com/demos/images/nature-5.jpg',
+    'https://swiperjs.com/demos/images/nature-6.jpg',
+    'https://swiperjs.com/demos/images/nature-7.jpg',
+    'https://swiperjs.com/demos/images/nature-8.jpg',
+  ]
+  return (
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y, EffectCoverflow]}
+      effect="coverflow"
+      grabCursor={true}
+      centeredSlides={true}
+      slidesPerView={3}
+      coverflowEffect={{
+        rotate: 0,
+        stretch: 1,
+        depth: 350,
+        modifier: 1,
+        slideShadows: true,
+      }}
+      navigation
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      className="carousel"
+    >
+      {slide_img.map((img, i) => {
+        return (
+          <SwiperSlide key={i}>
+            <img src={img} alt="" />
+          </SwiperSlide>
+        )
+      })}
+      ...
+    </Swiper>
+  )
+}
