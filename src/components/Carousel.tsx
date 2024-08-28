80507cdbcdbf287f6a119bc8f463c2ae671f6c3f
Keng Lim <keng@anewgo.com>
2024-08-27 20:23:00 -0400

    checkpoint changes. Display floor plan images

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -20,14 +20,60 @@ import './styles.css'
 export default function Carousel(): JSX.Element {
   // if you want to use array
   const slide_img = [
-    'https://swiperjs.com/demos/images/nature-1.jpg',
-    'https://swiperjs.com/demos/images/nature-2.jpg',
-    'https://swiperjs.com/demos/images/nature-3.jpg',
-    'https://swiperjs.com/demos/images/nature-4.jpg',
-    'https://swiperjs.com/demos/images/nature-5.jpg',
-    'https://swiperjs.com/demos/images/nature-6.jpg',
-    'https://swiperjs.com/demos/images/nature-7.jpg',
-    'https://swiperjs.com/demos/images/nature-8.jpg',
+    {
+      id: 1,
+      name: 'Aspen',
+      image:
+        'https://cdn.anewgo.com/app/demo/images/floorplans/shepard-fp1base_d254287ecd5c4fcd.png',
+    },
+    {
+      id: 2,
+      name: 'Oakridge',
+      image:
+        'https://cdn.anewgo.com/app/demo/images/floorplans/mendenhall-fp1base%20(demo)_85d23fe9cde8f03e.jpg',
+    },
+    {
+      id: 3,
+      name: 'Sycamore',
+      image:
+        'https://cdn.anewgo.com/app/demo/images/floorplans/oxford-a-fp1base_aae1661cb814f25f.png',
+    },
+    {
+      id: 4,
+      name: 'Hamilton',
+      image:
+        'https://cdn.anewgo.com/app/demo/images/floorplans/hamilton-a-fp1base_25485aecaf3d3cf7.jpg',
+    },
+    {
+      id: 5,
+      name: 'Magnolia',
+      image:
+        'https://cdn.anewgo.com/app/demo/images/floorplans/kamber-a-fp1base_8bf4d4eef2aab73e.png',
+    },
+    {
+      id: 6,
+      name: 'Dogwood',
+      image:
+        'https://cdn.anewgo.com/app/demo/images/floorplans/dogwood-a-fp1base_bb47b18bf9131112.png',
+    },
+    {
+      id: 7,
+      name: 'Cypress',
+      image:
+        'https://cdn.anewgo.com/app/demo/images/floorplans/garrison-a-fp1base_a2110e3170894878.jpg',
+    },
+    {
+      id: 8,
+      name: 'Juniper',
+      image:
+        'https://cdn.anewgo.com/app/demo/images/floorplans/blueridge-a-fp1base_b91e780546242757_d1d4e4af57f4ccc5.jpeg',
+    },
+    {
+      id: 9,
+      name: 'Cooper',
+      image:
+        'https://cdn.anewgo.com/app/demo/images/floorplans/cooper-fp1base_624adb3af1e3e695.jpg',
+    },
   ]
   return (
     <div className="swiper-container">
@@ -55,15 +101,13 @@ export default function Carousel(): JSX.Element {
         className="carousel"
       >
         {slide_img.map((img, i) => (
-          <SwiperSlide key={i} style={{ width: '300px' }}>
-            {' '}
-            {/* Set a fixed width for slides */}
-            <img
-              src={img}
-              alt=""
-              style={{ width: '100%', height: 'auto' }}
-            />{' '}
-            {/* Ensure images fit slide */}
+          <SwiperSlide key={i}>
+            <div className="carousel-slide">
+              <span className="carousel-slide-title">{img.name}</span>
+              <div className="carousel-slide-image-container">
+                <img src={img.image} alt={img.name} />
+              </div>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -5,9 +5,11 @@ function Category(): JSX.Element {
   const { categoryId } = useParams<{ categoryId: string }>()
 
   return (
-    <div className="w-full h-[calc(100vh-var(--nav-height))]">
+    <div className="w-full h-[calc(100vh-var(--nav-height))] flex flex-col items-center justify-center">
       <h1 className="text-2xl font-bold mb-4">Category: {categoryId}</h1>
-      <Carousel />
+      <div className="w-full max-w-4xl">
+        <Carousel />
+      </div>
     </div>
   )
 }
